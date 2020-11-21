46622dcae89077a8edbc54c71fad2e3d92ea8f27
Hongsun_Jang <hsjang0918@gmail.com>
2020-11-22 05:18:45 +0900

    rev6

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 var path = require('path');
 var qs = require('querystring');
+
+var bodyParser = require('body-parser');
+
 // 환경변수 설정
 var dotenv = require( 'dotenv');
 
 dotenv.config();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 app.get("/", (req,res)=>{
     res.send(`host: ${process.env.host} 비밀번호: ${process.env.password}`);
@@ -17,42 +23,75 @@ var db = mysql.createConnection({
   host     : process.env.host, 
   user     : process.env.user, 
   password : process.env.password,
-  database : process.env.db 
+  database : process.env.db, 
+  multipleStatements : true
 });
  
 db.connect(function(err){
   if(err) throw err;
-  console.log("You are connect!\n");
+  console.log("You are connect to DB!\n");
 });
- 
-// sql문으로 첫번째 인자를 주고, 두번째 인자로 callback function을 주면
-db.query('SELECT * FROM sample', function (error, results, fields) {
-  if (error) {
-		console.log(error);	
-	};
-  console.log(results);
+
+app.get('/api/diary', function(req, res){
+  var q = req.query;
+  var month = q.month;
+  var year = q.year;
+  db.query(`SELECT DAY(written_date) FROM contents WHERE YEAR(written_date) = ? AND MONTH(written_date) = ?`, [year, month], function(error, date){
+      res.json(date);
+    }
+  );
+});
+
+app.get('/api/diary/:diary_date', function(req,res){
+  var filtered_date = path.parse(req.params.diary_date).base;
+  db.query(`SELECT content FROM contents WHERE written_date = ?`, [date], 
+    function(error, date){
+      res.json(date);
+    }
+  );
 });
 
 //route, routing
-app.post('/create_process', function(request, response){
-    var post = request.body;
-    var title = post.title;
-    var description = post.description;
-    request.on('end', function(){
-        console.log(body);
-        var post = qs.parse(body);
-        var name = post.name;
-        var content = post.content;
-        db.query(`INSERT INTO contents (name, content) VALUES(?, ?)`,[post.name, post.content],
-          function(error, result){
-            if(error)throw error;
-            console.log(result);
-            response.writeHead(302, {Location: `/?id=${result.insertId}`});
-            response.end();
-        });
-    });
+app.post('/api/create_diary', function(req, res){
+    var post = req.body;
+    var date = post.date;
+    var content = post.content;
+    // AI 통신 code 들어갈 곳 
+    var feeling = 1;
+
+    var insert_query = `INSERT INTO contents(user_id, written_date, content) VALUES(0,?,?);`;
+    var select_query = `SELECT saying_id, saying_content, saying_author FROM sayings WHERE feeling = ?;`;
+    var total_query = insert_query + select_query;
+    db.query( total_query, [date, content, feeling],
+      function(error, result){
+        if(error)throw error;
+        res.json(result[1]);
+      }
+    );
 });
 
-app.listen(3000, function() {
-  console.log('Example app listening on port 3000!')
+app.post('/api/rating', function(req, res){
+  var post = req.body;
+  var date = post.date;
+  var saying_id = post.saying_id;
+  var pass = post.pass;
+  var total_query = `INSERT INTO ratings(content, saying_id, pass) VALUES((SELECT content FROM contents WHERE written_date = ?), ?, ?);`;
+  if(pass == '1'){
+    total_query = total_query + `UPDATE contents SET saying_id = ? WHERE written_date = ?;`;
+    db.query(total_query, [date, saying_id, pass, saying_id, date],
+      function(error, result){
+        if(error)throw error;
+      }
+    );
+  }
+  
+  db.query(total_query, [date, saying_id, pass],
+    function(error, result){
+      if(error)throw error;
+    }
+  );
+});
+
+app.listen(9000, function() {
+  console.log('Example app listening on port 9000!')
 });
